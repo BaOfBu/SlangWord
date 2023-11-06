389eb70ea198ba487f197b81fe4ab551720aad49
BaOfBu <111734920+BaOfBu@users.noreply.github.com>
2023-11-06 11:09:21 +0700

    SlangWord update

---
+++ src/SlangWord.java
@@ -0,0 +1,16 @@
+public class SlangWord {
+    private String _definition;
+    private String _slangword;
+    public String get_definition() {
+        return _definition;
+    }
+    public String get_slangword() {
+        return _slangword;
+    }
+    public void set_definition(String _definition) {
+        this._definition = _definition;
+    }
+    public void set_slangword(String _slangword) {
+        this._slangword = _slangword;
+    }
+}
